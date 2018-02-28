468867a0221b9ed4e815f69759e496a006e9878c
Thomas Brouard <contact@brrd.fr>
2018-02-28 23:54:22 +0100

    Inject pure-form script and stylesheet with JS

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const electron = require('electron');
 const settings = electron.remote.require('electron-settings');
 
+function init () {
+  const injectScript = (src) => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    document.head.appendChild(script);
+  };
+
+  const injectStylesheet = (src) => {
+    const link = document.createElement('link');
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.href = src;
+    document.head.appendChild(link);
+  };
+
+  // FIXME: move assets into a vendor dir (copy deps with npm run build)
+  injectStylesheet("../node_modules/pure-form/dist/pure-form.min.css");
+  injectScript("../node_modules/pure-form/dist/pure-form.min.js");
+}
+
 function createForm (srcPath, container = document.body) {
   const el = document.createElement('pure-form');
   el.setAttribute('id', 'electron-config-form');
@@ -12,6 +33,8 @@ function createForm (srcPath, container = document.body) {
 
 // TODO: add a way to define shema directly instead of using a filepath. Try using form.schema
 function run ({srcPath, container}) {
+  init();
+
   if (typeof container === "string") {
     container = document.querySelector(container);
   }
